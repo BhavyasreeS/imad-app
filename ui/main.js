e54faabbb0009dbb94eb2a9cdbde3cfea916b7a6
BhavyasreeS <sbhavya1515@gmail.com>
2017-08-14 22:51:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,14 +4,19 @@ button.onclick=function (){
     request.onreadystatechange=function(){
       if(request.readyState=== XMLHttpRequest.DONE){
         if(request.status===200){
-            var counter=request.responseText;
-            var span= document.getElementById('count');
-            span.innerHTML=counter.toString();
+           var names=request.responseText;
+           names=JSON.parse(names);
+            var list=' ';
+            for(var i=0;i< names.length;i++){
+                list+='<li>'+names[i]+'</li>';
+            }
+            var ul = document.getElementById('namelist');
+            ul.innerHTML=list;
         }  
       }
     };
   
-  request.open('Get','http://sbhavya1515.imad.hasura-app.io/counter',true);
+  request.open('Get','http://sbhavya1515.imad.hasura-app.io/submit-name?name='+name,true);
   request.send(null);
 };
 var nameinput=document.getElementById('name');
