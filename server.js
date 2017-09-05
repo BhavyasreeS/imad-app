5c4610aa0b0ec8183e822bc599e8773b36013bb1
BhavyasreeS <sbhavya1515@gmail.com>
2017-09-05 23:03:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var Pool = require('pg').Pool;
+var config = {
+    user:'sbhavya1515',
+    database:'sbhavya1515',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password: process.env.DB_PASSWORD,
+};
 var app = express();
 app.use(morgan('combined'));
 
@@ -27,6 +34,18 @@ app.get('/article-two',function(req,res){
 app.get('/article-three',function(req,res){
     res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
     
+});
+var Pool =new Pool(config);
+app.get('/test-db',function(req,res){
+    
+   pool.query('SELECT * FROM test',function(err,result){
+     if(err){
+         res.status(500).send(err.toString());
+     }  else{
+         res.send(JSON.stringify(result));
+     }
+       
+   }); 
 });
 
 app.get('/ui/style.css', function (req, res) {
